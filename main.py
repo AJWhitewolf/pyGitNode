a791d0b749e32d3bb375a74e79115f59fa8b3547
Adam Dunigan <adunigan@accruent.com>
2017-04-06 12:46:51 -0500

    update again

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 proc = subprocess.Popen(["pgrep", "node"], stdout=subprocess.PIPE)
-if len(proc) < 1:
+if len(proc.stdout) < 1:
     subprocess.Popen(["node", "/home/pi/Projects/nodehooks/bin/www"])
 
 
