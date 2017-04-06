b0ddd6723c2d5dd5f5f95af4468099ebedc514d1
Adam Dunigan <adunigan@accruent.com>
2017-04-06 14:00:28 -0500

    Console logging

--- main.py
+++ main.py
@@ -11,12 +11,10 @@ BUFFER_SIZE = 60
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
-proc = subprocess.Popen(["pgrep", "node"], stdout=subprocess.PIPE)
-for pid in proc.stdout:
-    print pid
-# if len(proc.stdout) < 1:
-#     subprocess.Popen(["node", "/home/pi/Projects/nodehooks/bin/www"])
 
+print "Starting Node API server..."
+subprocess.Popen(["node", "/home/pi/Projects/nodehooks/bin/www"])
+print "Node API server started, output piped here."
 
 while True:
     conn, addr = s.accept()
@@ -28,9 +26,13 @@ while True:
         if data == "git restart":
             proc = subprocess.Popen(["pgrep", "node"], stdout=subprocess.PIPE)
             for pid in proc.stdout:
+                print "Killing Node process, PID: ", pid
                 os.kill(int(pid), signal.SIGKILL)
+            print "Performing Git Pull..."
             g = git.cmd.Git('/home/pi/Projects/nodehooks')
             g.pull()
+            print "Git Pull complete, restarting Node API server..."
             subprocess.Popen(["node", "/home/pi/Projects/nodehooks/bin/www"])
+            print "Node API server restarted, output piped here."
 
     conn.close()
